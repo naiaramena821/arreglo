84de90e6b589a29e459c95a564aadaf01ba14b47
naiaramena821 <54364209+naiaramena821@users.noreply.github.com>
2019-09-25 10:01:22 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+// 1 imprimir los elementos de un arreglo
+/*
+void imprimir(int arr[],int tam)
+{
+    for (int i=0;i < tam;i++){
+        cout << arr[i] << endl;
+    }
+}
+int main()
+{
+    int x[5];
+    for (int i=0;i<5;i++){
+    cin>> x[i];
+    }
+    imprimir(x,5);
+    return 0;
+}
+
+//2 función para retornar la suma de los elementos de un arreglo
+//recursiva
+int sumar(int arr[],int tam)
+{
+    if (tam==1){
+        return arr[0];
+    }
+    else{
+    return arr[tam-1]+sumar(arr,tam-1);
+}
+}
+int main()
+{
+    int tam;
+    cin >> tam;
+    int arr[tam];
+    for (int i=0;i<tam;i++){
+            cin >> arr[i];
+    }
+    cout << sumar (arr,tam) << endl;
+}
+*/
+
+//iterativa
+int sumai(arr[],tam)
+{
+    int sum=0;
+}
